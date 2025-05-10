34a0ce2cf016a14c862a596d2c4c33a560ba5c0d
isvargasm <ismael.vargas@storecheck.com>
2025-05-10 09:51:22 -0600

    agregar analytics de vercel

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import KpiFormCarrusel from './components/forms/KpiFormCarousel'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import TutorialHome from './pages/TutorialHome';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="min-h-screen bg-bg dark:bg-bg-dark text-text dark:text-text-dark transition-colors">
       <div className="">
+      <Analytics/>
         <Router>
           <Navbar />
           <Routes>
